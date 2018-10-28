f34a0ef236e6437b34f46f4a98519d768982fc8d
xyy_xde <bewantbe@gmail.com>
2018-10-28 03:57:46 -0400

    Running model

--- ann_orthpolyfit.py
+++ ann_orthpolyfit.py
@@ -5,17 +5,21 @@ exec(open('/home/xyy/Documents/research/poly-orth/ex/weighted_orthpoly_solver.py
 
 n_poly_order = 100
 V = orthpoly_coef(None, 'legendre', n_poly_order)
-x_sample = sin( pi * (-n_poly_order/2 + arange(n_poly_order+1))/(n_poly_order+1) )
+#x_sample = sin( pi * (-n_poly_order/2 + arange(n_poly_order+1))/(n_poly_order+1) )
+x_sample = lege.legroots(1*(arange(n_poly_order+2)==n_poly_order+1))
 get_fq_coef = lambda f: np.linalg.solve(V, f(x_sample))
 #get_fq_coef = lambda f: orthpoly_coef(f, 'legendre', n_poly_order)
 #get_fq_coef = lambda f: chb.Chebyshev.interpolate(f, n_poly_order).coef
 
-cost_l = lambda pm: cost_flat(ans_xy, pm, layer_dims, active_function = 'exptip')
+#
+act_fun = 'tanh'
+
+cost_l = lambda pm: cost_flat(ans_xy, pm, layer_dims, active_function = act_fun)
 cost_g = ag.jacobian(cost_l)
 cost_h = ag.hessian (cost_l)
 
 # ANN function
-ann_theta_x = lambda pm, x: np.squeeze(net_predict(x, wb_flat2layers(pm, layer_dims), active_function = 'exptip'))
+ann_theta_x = lambda pm, x: np.squeeze(net_predict(x, wb_flat2layers(pm, layer_dims), active_function = act_fun))
 ann_g = ag.jacobian(ann_theta_x, argnum=0)
 ann_h = ag.hessian (ann_theta_x, argnum=0)
 
@@ -24,16 +28,9 @@ layer_dims = [1, 2000, 1]
 pm0 = 1.0 * npr.randn(n_ann_param(layer_dims))
 
 # a glance of the ANN function
-figure(11);  clf()
-s_x = np.linspace(-1, 1, 100)[:, np.newaxis]
-plot(s_x, ann_theta_x(pm0, s_x))
-
-cb1 = chb.Chebyshev.interpolate(lambda x: ann_theta_x(pm0, x[:,np.newaxis]), 100)
-
-figure(13); clf()
-plt.semilogy(np.abs(cb1.coef), '-o')
-plt.ylim([1e-10, 10])
-pic_output('cb_coef_init', None)
+#figure(11);  clf()
+#s_x = np.linspace(-1, 1, 100)[:, np.newaxis]
+#plot(s_x, ann_theta_x(pm0, s_x))
 
 def gd_callback(params, t, g):
     loss = cost_l(params)
@@ -64,8 +61,8 @@ ans_xy = get_training_set(n_sample, fun)
 fun_cb = get_fq_coef(fun)
 
 flat_wb = pm0
-n_iter = 10000
-step_size = 0.0005
+n_iter = 100000
+step_size = 0.0002
 wb_end = adam(lambda pm, k: cost_g(pm), flat_wb, callback=gd_callback,
               num_iters=n_iter, step_size=step_size)
 
